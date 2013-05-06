b8e2d4277c7cde30d1718be6689633374481b4b7
Miguel <miguel@hubii.com>
2013-05-06 13:53:56 +0200

    Some code improvements. Fixing error with the animation of the dots.
    Fixed the error that show a wrong animation for the dots with the time.
    Some improvements for the compression of the file.
    Simpler code.
    Added some comments.

--- dot.js
+++ dot.js
@@ -2,10 +2,28 @@
 {
   $.fn.loadingdots = function( options )
   {
-    var settings, i;
-    settings = $.extend( {}, { duration : 100 }, options );
-    i = 0;
+    var i = 0, settings = $.extend( {}, { duration : 250 }, options ),
 
+    bucle = function() {
+      var $el = $( this ), cycle, timing = i * settings.duration, first = true;
+      i++;
+
+      cycle = function()
+      {
+        // if it's not the first time the cycle is called for a dot then the timing fired is 0
+        if ( !first )
+          timing = 0;
+        else
+          first = false;
+        // delay the animation the timing needed, and then make the animation to fadeIn and Out the dot to make the effect
+        $el.delay( timing )
+          .fadeTo( 1000, 0.4 )
+          .fadeTo( 1000, 0, cycle );
+      };
+
+      cycle( first );
+    };
+    // for every element where the plugin was called we create the loading dots html and start the animations
     return this.each( function()
     {
       $( this )
@@ -14,16 +32,5 @@
         .each( bucle );
     });
 
-    function bucle() {
-      var $el = $( this ), cycle, timing;
-      timing = i * settings.duration;
-      i++;
-      (cycle = function() {
-         $el.delay( timing )
-              .fadeTo( 'slow', 0.4 )
-              .delay( timing )
-              .fadeTo( 'slow', 0, cycle );
-      })();
-    }
   };
 })(jQuery);
