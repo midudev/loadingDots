3d90bcef2070d7b9430a7a2d644905c63c57baee
Miguel Ángel Durán <miduga@gmail.com>
2021-11-12 21:21:26 +0100

    Hello world after ten years

--- dot.js
+++ dot.js
@@ -1,36 +1,43 @@
-(function($)
-{
-  $.fn.loadingdots = function( options )
-  {
-    var i = 0, settings = $.extend( {}, { duration : 250 }, options ),
+if (typeof window.jQuery === 'undefined') {
+  console.info('jQuery is not loaded and it\'s needed for Loading Dots Plugin')
+} else {
+  const DEFAULT_CONFIG = { duration: 250 }
+  const dotsHtml = '<span class="dot"></span><span class="dot"></span><span class="dot"></span>'
+  const dotsClassName = '.dot'
 
-    bucle = function() {
-      var $el = $( this ), cycle, timing = i * settings.duration, first = true;
-      i++;
+  ;(function ($) {
+    $.fn.loadingdots = function (options) {
+      let i = 0
+      const settings = { ...DEFAULT_CONFIG, options }
 
-      cycle = function()
-      {
-        // if it's not the first time the cycle is called for a dot then the timing fired is 0
-        if ( !first )
-          timing = 0;
-        else
-          first = false;
-        // delay the animation the timing needed, and then make the animation to fadeIn and Out the dot to make the effect
-        $el.delay( timing )
-          .fadeTo( 1000, 0.4 )
-          .fadeTo( 1000, 0, cycle );
-      };
+      const bucle = function () {
+        const $el = $(this)
+        let timing = i * settings.duration
+        let first = true
 
-      cycle( first );
-    };
-    // for every element where the plugin was called we create the loading dots html and start the animations
-    return this.each( function()
-    {
-      $( this )
-        .html( '<span class="dot"></span><span class="dot"></span><span class="dot"></span>' )
-        .find( '.dot' )
-        .each( bucle );
-    });
+        i++
 
-  };
-})(jQuery);
+        const cycle = function () {
+          // if it's not the first time the cycle is called for a dot then the timing fired is 0
+          if (!first) timing = 0
+          else first = false
+
+          // delay the animation the timing needed, and then make the animation to fadeIn and Out the dot to make the effect
+          $el.delay(timing)
+            .fadeTo(1000, 0.4)
+            .fadeTo(1000, 0, cycle)
+        }
+
+        cycle(first)
+      }
+
+      // for every element where the plugin was called we create the loading dots html and start the animations
+      return this.each(function () {
+        $(this)
+          .html(dotsHtml)
+          .find(dotsClassName)
+          .each(bucle)
+      })
+    }
+  })(window.jQuery)
+}
